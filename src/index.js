9b71d27b99d9ebdcb45d43780a124b1045861d3c
Тим Бад <timurbadalbaev19@gmail.com>
2022-01-16 19:13:55 +0300

    refactor: task solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 module.exports = function toReadable (number) {
-
-// const toReadable = (number) => {
     let n = String(number);
     let numbers = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
     let digits = ['zero', 'hundred', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
@@ -9,26 +7,21 @@ module.exports = function toReadable (number) {
     switch (n.length) {
         case 3:
             rezult += `${numbers[n[0]]} ${digits[1]}`;
-            
         
         case 2:
             if (((n.slice(-2)) < 20) ) {
                 rezult += ` ${numbers[+(n.slice(-2))]}`
-                
                 break;
             } else {
                 rezult += ` ${digits[n.slice(-2,-1)]}`
-                
             };
         
         case 1:
             if (n == 0) {
                 rezult +='zero';
-                
-                break
+                break;
             } else {
                 rezult += ` ${numbers[n.slice(-1)]}`
-                
                 break;
             }
         
@@ -36,6 +29,5 @@ module.exports = function toReadable (number) {
             return 'Not a number';
     }
     
-    // console.log(n, rezult.trim());
     return rezult.trim();
 }
